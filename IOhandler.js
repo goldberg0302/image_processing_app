1819448c8258cb99633bd4e823067ae94fe1b15c
goldberg0302 <51843759+goldberg0302@users.noreply.github.com>
2022-07-07 00:30:59 -0700

    second change

--- IOhandler.js
+++ IOhandler.js
@@ -3,11 +3,18 @@
  * File Name: IOhandler.js
  * Description: Collection of functions for files input/output related operations
  * 
- * Created Date: 
- * Author: 
+ * Created Date: July 04, 2022
+ * Author: Shota Nakajo
+ * Student ID: A00888785
+ * Course: COMP3012
  * 
  */
 
+const { resolve } = require('path');
+const { on } = require('stream');
+const { createReadStream, createWriteStream} = require('fs');
+
+// const { resolve } = require('path');
 const unzipper = require('unzipper'),
   fs = require("fs"),
   PNG = require('pngjs').PNG,
@@ -23,10 +30,23 @@ const unzipper = require('unzipper'),
  */
 const unzip = (pathIn, pathOut) => {
   return new Promise((resolve,reject) => {
-    fs.createReadStream(pathIn) 
+    fs.createReadStream(pathIn)
     .pipe(unzipper.Extract({path:pathOut}))
+    .on('error', reject)
+    .on('finish',resolve)
   });
 }
+
+// try it without wrapping with promise (read documentation)
+// const unzip = (pathIn, pathOut) => {
+//   fs.createReadStream(pathIn)
+//     .pipe(unzipper.Parse())
+//     .on('entry', function (entry) {
+//       unzipper.Extract({path:pathOut})
+//     })
+//     .promise();
+// }
+
 
 /**
  * Description: read all the png files from given directory and return Promise containing array of each png file path 
@@ -34,8 +54,16 @@ const unzip = (pathIn, pathOut) => {
  * @param {string} path 
  * @return {promise}
  */
-const readDir = dir => {
-
+const readDir = (dir) => {
+   return new Promise((resolve,reject) => {
+    fs.readdir(dir, (err, files) =>{
+      if (err) {
+        reject(err);
+      } else {
+        resolve(files.filter(file => path.extname(file) === '.png'));
+      }
+    })
+  });
 };
 
 /**
@@ -46,10 +74,39 @@ const readDir = dir => {
  * @param {string} pathProcessed 
  * @return {promise}
  */
-const grayScale = (pathIn, pathOut) => {
+const grayScale = (file, pathProcessed) => {
+  return new Promise((resolve, reject) => {
+    fs.createReadStream(path.resolve(__dirname,'unzipped',file))
+    .pipe(
+      new PNG({
+        filterType: 4,
+      })
+    )
+    
+    .on('parsed', function(){
+      for (var y = 0; y < this.height; y++) {
+          for (var x = 0; x < this.width; x++) {
+            var idx = (this.width * y + x) << 2;
 
+            // change to grayscale
+            let luma_component = 0.2627 * this.data[idx]
+                  + 0.6780 * this.data[idx + 1]
+                  + 0.0593 * this.data[idx + 2];
+            this.data[idx] = luma_component;
+            this.data[idx + 1] = luma_component;
+            this.data[idx + 2] = luma_component;
+
+            // and reduce opacity
+            // this.data[idx + 3] = this.data[idx + 3] >> 1;
+          }
+        }
+        this.pack() 
+        .pipe(fs.createWriteStream(path.resolve(__dirname,pathProcessed, file)))
+        .on('error', function(e){console.log(e)})
+    })
+    .on('error', function(e){console.log(e)})
+  });
 };
-
 
 
 module.exports = {
--- main.js
+++ main.js
@@ -3,22 +3,24 @@
  * File Name: main.js
  * Description: 
  * 
- * Created Date: June 30, 2022
+ * Created Date: July 04, 2022
  * Author: Shota Nakajo
  * Student ID: A00888785
+ * Course: COMP3012
  * 
  */
+const path = require('path');
 
 const IOhandler = require("./IOhandler"),
   zipFilePath = `${__dirname}/myfile.zip`,
   pathUnzipped = `${__dirname}/unzipped`,
   pathProcessed = `${__dirname}/grayscaled`;
 
-  IOhandler.unzip("", "")
-  .then(()=>IOhandler.readDir(pathUnzipped))
-  // .then((filePaths)=> console.log(filePaths)) ["*.png", "*.png"]
+  IOhandler.unzip(zipFilePath, pathUnzipped)
+  .then(()=>IOhandler.readDir(pathUnzipped)) 
+  // .then((filePaths)=> console.log(filePaths)) // ["*.png", "*.png"]
   .then((filePaths)=>
-    filePaths.forEach(path=>IOhandler.grayScale(path,pathProcessed))
+    filePaths.forEach(path => IOhandler.grayScale(path,pathProcessed))
   )
   .catch(err=>console.log(err))
 
